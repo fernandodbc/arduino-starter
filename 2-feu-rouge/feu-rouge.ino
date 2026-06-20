a495cd01870378a3a7da5b7a463466b6b6633bd9
FDBC <fernando@dbce.fr>
2026-06-20 08:29:02 +0200

    ajouter le projet thermometre

--- 2-feu-rouge/feu-rouge.ino
+++ 2-feu-rouge/feu-rouge.ino
@@ -1,7 +1,8 @@
-int etatDeLInterrupteur = 0;
+int etatDeLInterrupteur = LOW;
+bool lancerLaBoucle = false;
 
 void setup() {
-  // ce code ne s'excute que quand la carte s'allume
+  // ce code ne s'execute que quand la carte s'allume
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
@@ -14,17 +15,29 @@ void loop() {
   // lit le pin digital numéro 2 pour voir si l'electricité passe ou pas
   // s'il y a de la tension ca va retourner HIGH (1), sinon LOW (0)
   etatDeLInterrupteur = digitalRead(2);
-  if (etatDeLInterrupteur == HIGH) {
+
+  if (etatDeLInterrupteur == HIGH && lancerLaBoucle == true) {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    lancerLaBoucle = false;
+    delay(250);
+  } else if (etatDeLInterrupteur == HIGH && lancerLaBoucle == false) {
+    lancerLaBoucle = true;
+    delay(250);
+  }
+  
+
+  if (lancerLaBoucle) {
     digitalWrite(3, HIGH);
-    delay(5000);
+    delay(8000);
     digitalWrite(3, LOW);
     digitalWrite(4, HIGH);
     delay(1500);
     digitalWrite(4, LOW);
     digitalWrite(5, HIGH);
-    delay(5000);
+    delay(4000);
     digitalWrite(5, LOW);
   }
-  exit(1);
 
 }
