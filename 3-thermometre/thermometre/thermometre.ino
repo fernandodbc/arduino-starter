1997fdf76fb88230f2bcbcf21b5edc8468740c47
FDBC <fernando@dbce.fr>
2026-06-20 08:30:10 +0200

    code styling

--- 3-thermometre/thermometre/thermometre.ino
+++ 3-thermometre/thermometre/thermometre.ino
@@ -2,50 +2,50 @@ const int sensorPin = A0;
 const float baselineTemp = 30.0;
 
 void setup() {
-Serial.begin(9600); // ouvrir un port série
-for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
-  pinMode(pinNumber, OUTPUT);
-  digitalWrite(pinNumber, LOW);
-}
+  Serial.begin(9600); // ouvrir un port série
+  for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
 }
 
 void loop() {
-int sensorVal = analogRead(sensorPin);
-Serial.print("Sensor Value: ");
-Serial.print(sensorVal);
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
 
-// convertir la lecture du CAN en tension
-float voltage = (sensorVal / 1024.0) * 5.0;
+  // convertir la lecture du CAN en tension
+  float voltage = (sensorVal / 1024.0) * 5.0;
 
-Serial.print(", Volts: ");
-Serial.print(voltage);
-Serial.print(", degrés C: ");
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", degrés C: ");
 
-//  convertir la tension en température en degrés
-float temperature = (voltage - 0.5) * 100;
-Serial.println(temperature);
+  //  convertir la tension en température en degrés
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
 
-//  allumer les led en fonction de la température de base
-if (temperature < baselineTemp + 2) {
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
+  //  allumer les led en fonction de la température de base
+  if (temperature < baselineTemp + 2) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
 
-} else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
 
-} else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
 
-} else if (temperature >= baselineTemp + 6) {
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-}
+  } else if (temperature >= baselineTemp + 6) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
 
-delay(1);
+  delay(1);
 }
